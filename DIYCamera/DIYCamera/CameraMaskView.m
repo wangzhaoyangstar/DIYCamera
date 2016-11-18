64549b9141d43ef3ba64c8af4430e30c5013e867
wangzhaoyangstar <wangzhaoyangstar@163.com>
2016-11-18 10:42:46 +0800

    绘制相机快门的直线

--- DIYCamera/DIYCamera/CameraMaskView.m
+++ DIYCamera/DIYCamera/CameraMaskView.m
@@ -19,13 +19,29 @@
 
 - (void)drawRect:(CGRect)rect {
     
-    //绘制一个矩形.填充指定的颜色
-    [[UIColor cz_colorWithHex:0x24282e] setFill];
-    UIBezierPath *path = [UIBezierPath bezierPathWithRect:rect];
-    [path fill];
-    
+    {//绘制一个矩形.填充指定的颜色
+        [[UIColor cz_colorWithHex:0x24282e] setFill];
+        UIBezierPath *path = [UIBezierPath bezierPathWithRect:rect];
+        [path fill];
+        
+    }
+    {
+        //绘制中间的直线
+        UIBezierPath *path = [UIBezierPath bezierPath];
+        //区分左右快门
+        if (_imageView.frame.origin.x > 0) {
+            //左侧画线
+            [path moveToPoint:CGPointMake(rect.size.width, 0)];
+            [path addLineToPoint:CGPointMake(rect.size.width, rect.size.height)];
+        } else{
+            //右侧画线
+            [path moveToPoint:CGPointMake(0, 0)];
+            [path addLineToPoint:CGPointMake(0, rect.size.height)];
+        }
+        [[UIColor cz_colorWithHex:0x1a1a1a] setStroke];
+        [path stroke];
+    }
     CGContextClearRect(UIGraphicsGetCurrentContext(),_imageView.frame);
-    
 }
 
 
